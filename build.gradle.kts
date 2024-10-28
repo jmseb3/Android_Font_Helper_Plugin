582de9a19b628bf43e11eeb1e4c05452fd06205f
jmseb3 <jmseb3@naver.com>
2024-10-28 15:04:21 +0900

    Fix Until version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.wonddak"
-version = "1.3.0"
+version = "1.3.1"
 
 repositories {
     mavenCentral()
@@ -35,7 +35,8 @@ intellijPlatform {
     pluginConfiguration {
         name = "FontHelper"
         ideaVersion.sinceBuild.set("223")
-        ideaVersion.untilBuild.set("242.21829.*")
+//        ideaVersion.untilBuild.set("242.21829.*")
+        ideaVersion.untilBuild.set(provider { null })
     }
 
     signing {

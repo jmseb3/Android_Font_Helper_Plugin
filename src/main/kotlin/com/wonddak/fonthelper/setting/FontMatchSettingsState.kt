4d27fab82c81df70edd37a6b8b61c23a0f288dea
jmseb3 <jmseb3@naver.com>
2025-05-12 16:51:40 +0900

    edit filter

--- src/main/kotlin/com/wonddak/fonthelper/setting/FontMatchSettingsState.kt
+++ src/main/kotlin/com/wonddak/fonthelper/setting/FontMatchSettingsState.kt
@@ -1,23 +1,65 @@
 package com.wonddak.fonthelper.setting
 
 data class FontMatchSettingsState(
-    var thinKeywords: List<String> = listOf("-Thin", "-thin"),
-    var extraLightKeywords: List<String> = listOf("-ExtraLight", "-extraLight"),
-    var lightKeywords: List<String> = listOf("-Light", "-light"),
-    var regularKeywords: List<String> = listOf("-Regular", "-regular"),
-    var mediumKeywords: List<String> = listOf("-Medium", "-medium"),
-    var semiBoldKeywords: List<String> = listOf("-SemiBold", "-semiBold"),
-    var boldKeywords: List<String> = listOf("-Bold", "-bold"),
-    var extraBoldKeywords: List<String> = listOf("-ExtraBold", "-extraBold"),
-    var blackKeywords: List<String> = listOf("-Black", "-black"),
-    var italicKeywords: List<String> = listOf("-Italic", "-italic"),
-    var thinItalicKeywords: List<String> = listOf("-ThinItalic", "-thinItalic"),
-    var extraLightItalicKeywords: List<String> = listOf("-ExtraLightItalic", "-extraLightItalic"),
-    var lightItalicKeywords: List<String> = listOf("-LightItalic", "-lightItalic"),
-    var regularItalicKeywords: List<String> = listOf("-RegularItalic", "-regularItalic"),
-    var mediumItalicKeywords: List<String> = listOf("-MediumItalic", "-mediumItalic"),
-    var semiBoldItalicKeywords: List<String> = listOf("-SemiBoldItalic", "-semiBoldItalic"),
-    var boldItalicKeywords: List<String> = listOf("-BoldItalic", "-boldItalic"),
-    var extraBoldItalicKeywords: List<String> = listOf("-ExtraBoldItalic", "-extraBoldItalic"),
-    var blackItalicKeywords: List<String> = listOf("-BlackItalic", "-blackItalic")
-)
+    var thinKeywords: List<String> = listOf("-thin"),
+    var extraLightKeywords: List<String> = listOf("-extralight"),
+    var lightKeywords: List<String> = listOf("-light"),
+    var regularKeywords: List<String> = listOf("-regular"),
+    var mediumKeywords: List<String> = listOf("-medium"),
+    var semiBoldKeywords: List<String> = listOf("-semibold"),
+    var boldKeywords: List<String> = listOf("-bold"),
+    var extraBoldKeywords: List<String> = listOf("-extraBold"),
+    var blackKeywords: List<String> = listOf("-black"),
+
+    var thinItalicKeywords: List<String> = listOf("-thinitalic"),
+    var extraLightItalicKeywords: List<String> = listOf("-extralightitalic"),
+    var lightItalicKeywords: List<String> = listOf("-lightitalic"),
+    var regularItalicKeywords: List<String> = listOf("-italic"),
+    var mediumItalicKeywords: List<String> = listOf("-mediumitalic"),
+    var semiBoldItalicKeywords: List<String> = listOf("-semibolditalic"),
+    var boldItalicKeywords: List<String> = listOf("-bolditalic"),
+    var extraBoldItalicKeywords: List<String> = listOf("-extrabolditalic"),
+    var blackItalicKeywords: List<String> = listOf("-blackitalic")
+) {
+    /**
+     * @return Pair<isItalic, fontWeightIndex> or null if no match
+     */
+    fun checkType(fileName: String) : Pair<Boolean,Int>? {
+        val italicMap = mapOf(
+            extraLightItalicKeywords to 1,
+            extraBoldItalicKeywords to 7,
+            semiBoldItalicKeywords to 5,
+            thinItalicKeywords to 0,
+            lightItalicKeywords to 2,
+            regularItalicKeywords to 3,
+            mediumItalicKeywords to 4,
+            boldItalicKeywords to 6,
+            blackItalicKeywords to 8
+        )
+        for ((keywords,index) in italicMap) {
+            if (keywords.any { fileName.contains(it, ignoreCase = true) }) {
+                return true to index
+            }
+        }
+
+        val regularMap = mapOf(
+            extraLightKeywords to 1,
+            extraBoldKeywords to 7,
+            semiBoldKeywords to 5,
+            thinKeywords to 0,
+            lightKeywords to 2,
+            regularKeywords to 3,
+            mediumKeywords to 4,
+            boldKeywords to 6,
+            blackKeywords to 8
+        )
+
+        for ((keywords, index) in regularMap) {
+            if (keywords.any { fileName.contains(it, ignoreCase = true) }) {
+                return false to index
+            }
+        }
+
+        return  null
+    }
+}
--- src/main/kotlin/com/wonddak/fonthelper/widget/DragDropFiles.kt
+++ src/main/kotlin/com/wonddak/fonthelper/widget/DragDropFiles.kt
@@ -1,26 +1,36 @@
 package com.wonddak.fonthelper.widget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileUpload
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
 import androidx.compose.ui.draganddrop.DragData
 import androidx.compose.ui.draganddrop.dragData
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.wonddak.fonthelper.util.FontUtil
-import com.wonddak.fonthelper.util.FontUtil.FontTypeConst
+import com.wonddak.fonthelper.setting.FontMatchSettingsService
+import com.wonddak.fonthelper.setting.FontMatchSettingsState
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -28,21 +38,20 @@ fun DragDropFiles(
     updateNormalFontList: (Int, String) -> Unit,
     updateItalicFontList: (Int, String) -> Unit,
 ) {
-    val fontWeightMap = mapOf(
-        FontTypeConst.EXTRA_LIGHT.lowercase() to 1,
-        FontTypeConst.EXTRA_BOLD.lowercase() to 7,
-        FontTypeConst.SEMI_BOLD.lowercase() to 5,
-        FontTypeConst.THIN.lowercase() to 0,
-        FontTypeConst.LIGHT.lowercase() to 2,
-        FontTypeConst.NORMAL.lowercase() to 3,
-        FontTypeConst.REGULAR.lowercase() to 3,
-        FontTypeConst.MEDIUM.lowercase() to 4,
-        FontTypeConst.BOLD.lowercase() to 6,
-        FontTypeConst.BLACK.lowercase() to 8
-    )
+    var isHover by remember {
+        mutableStateOf(false)
+    }
+
+    val service = FontMatchSettingsService.getInstance()
+    val settings: FontMatchSettingsState = service.state
 
     val callback = remember {
         object : DragAndDropTarget {
+            override fun onStarted(event: DragAndDropEvent) {
+                super.onStarted(event)
+                isHover = true
+            }
+
             override fun onDrop(event: DragAndDropEvent): Boolean {
                 // Parse received data
                 val data = event.dragData()
@@ -52,13 +61,7 @@ fun DragDropFiles(
                         if (!filePath.endsWith(".ttf") && !filePath.endsWith(".otf")) continue
 
                         val fileName = filePath.trimEnd('/').substringAfterLast('/').lowercase()
-                        val isItalic = fileName.contains(FontUtil.ITALIC.lowercase())
-
-                        val matchedEntry = fontWeightMap.entries.firstOrNull { (type, _) ->
-                            fileName.contains(type)
-                        }
-
-                        matchedEntry?.let { (_, weight) ->
+                        settings.checkType(fileName)?.let { (isItalic, weight) ->
                             if (isItalic) updateItalicFontList(weight, filePath)
                             else updateNormalFontList(weight, filePath)
                         }
@@ -66,15 +69,28 @@ fun DragDropFiles(
                 }
                 return false
             }
+
+            override fun onEnded(event: DragAndDropEvent) {
+                super.onEnded(event)
+                isHover = false
+            }
         }
     }
-    Column(
+    Box(
         modifier = Modifier
-            .size(200.dp)
+            .fillMaxWidth(0.8f)
+            .heightIn(min = 150.dp)
             .border(
                 width = 1.dp,
                 color = MaterialTheme.colors.primary,
                 shape = RoundedCornerShape(10.dp),
+            )
+            .background(
+                color = when {
+                    isHover -> MaterialTheme.colors.primary.copy(alpha = 0.05f)
+                    else -> Color.Transparent
+                },
+                shape = MaterialTheme.shapes.small,
             )
             .dragAndDropTarget(
                 shouldStartDragAndDrop = { event ->
@@ -82,8 +98,20 @@ fun DragDropFiles(
                 }, target = callback
             )
     ) {
-        Text(
-            text = "Drop files here!",
-        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Filled.FileUpload,
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
+            )
+            Text(
+                text = "Drag & drop Font Files",
+            )
+        }
     }
 }
